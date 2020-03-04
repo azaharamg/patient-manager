ae0635a96e7e048b03ca142ca39660610d419d1e
Azahara <azaharamaga@gmail.com>
2020-03-04 09:43:13 +0100

    Get data from inputs to update state

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,20 +1,52 @@
-import React, { Fragment } from 'react';
+import React, { Fragment, useState } from 'react';
 
 const Form = () => {
+  // Create State
+  const [appointment, updateAppointment] = useState({
+    pet: '',
+    owner: '',
+    date: '',
+    time: '',
+    symptom: ''
+  });
+
+  const updateState = e => {
+    updateAppointment({
+      ...appointment,
+      [e.target.name]: e.target.value
+    });
+  };
+
+  const { pet, owner, date, time, symptom } = appointment;
+
   return (
     <Fragment>
       <h2>Crear Cita</h2>
       <form>
         <label>Nombre Mascota</label>
-        <input type='text' name='pet' className='u-full-width' placeholder='Nombre de la mascota...' />
+        <input
+          type='text'
+          name='pet'
+          className='u-full-width'
+          placeholder='Nombre de la mascota...'
+          onChange={updateState}
+          value={pet}
+        />
         <label>Nombre Dueño</label>
-        <input type='text' name='owner' className='u-full-width' placeholder='Nombre del dueño...' />
+        <input
+          type='text'
+          name='owner'
+          className='u-full-width'
+          placeholder='Nombre del dueño...'
+          onChange={updateState}
+          value={owner}
+        />
         <label>Fecha</label>
-        <input type='date' name='date' className='u-full-width' />
+        <input type='date' name='date' className='u-full-width' onChange={updateState} value={date} />
         <label>Hora</label>
-        <input type='time' name='time' className='u-full-width' />
+        <input type='time' name='time' className='u-full-width' onChange={updateState} value={time} />
         <label>Síntomas</label>
-        <textarea className='u-full-width' name='symptom'></textarea>
+        <textarea className='u-full-width' name='symptom' onChange={updateState} value={symptom}></textarea>
         <button type='submit' className='u-full-width button-primary'>
           Agregar Cita
         </button>
